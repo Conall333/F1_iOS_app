4463cbc98c716cd446955d43d063d770f5e3e378
conall333 <conallmccarthy@outlook.com>
2019-02-05 10:10:28 +0000

    Initial Commit

---
+++ Person Information/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person Information
+//
+//  Created by Conall Mc Carthy on 05/02/2019.
+//  Copyright © 2019 Conall Mc Carthy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Person InformationTests/Person_InformationTests.swift
@@ -0,0 +1,34 @@
+//
+//  Person_InformationTests.swift
+//  Person InformationTests
+//
+//  Created by Conall Mc Carthy on 05/02/2019.
+//  Copyright © 2019 Conall Mc Carthy. All rights reserved.
+//
+
+import XCTest
+@testable import Person_Information
+
+class Person_InformationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
